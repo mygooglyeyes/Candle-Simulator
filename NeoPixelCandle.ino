59816865b61c5960aaed75398bc5684ba369bc4c
Brett Stewart <mygooglyeyes@users.noreply.github.com>
2014-12-13 16:19:26 -0800

    Update NeoPixelCandle.ino

--- NeoPixelCandle.ino
+++ NeoPixelCandle.ino
@@ -19,21 +19,22 @@ void setup()
 
 void loop()
 {
-	wait = random(60, 80);  //set a random wait period
+	wait = random(100, 120);  //set a random wait period
 	randpix = random(0, numpix + 1); //choose a random number of pixels
 	color = random(0, 2); //Pick either yellow or orange
-	for (int i = 0; i < numpix / 3; i = i++) //so it leaves a certain number of yellow pixels on (number of pixels/3)
+	for (int i = 0; i < numpix / 2; i = i++) //so it leaves a certain number of yellow pixels on (number of pixels/3)
 	{
 		strip.setPixelColor(i, 255, 130, 0);  //set the number of pixels to turn on and color value (yellowish)
 	}
 	strip.show(); //turn pixels on
+
 	if (color == 0)  //if red was chosen
 	{
 		flickred(strip.Color(115, 50, 0), wait, randpix); // call flickred and pass it the red (orangeish) color values - change values to change color
 	}
 	else  // otherwise use yellow
 	{
-		flickYellow(strip.Color(255, 130, 0), wait, randpix); // call flickYellow and pass it the yellow color values (change values to change color), and wait time and random pixel count
+		flickYellow(strip.Color(180, 80, 0), wait, randpix); // call flickYellow and pass it the yellow color values (change values to change color), and wait time and random pixel count
 	}
 }  //end main loop
 
@@ -45,6 +46,7 @@ void flickred(uint32_t c, uint8_t wait, uint8_t p)
 	{
 		strip.setPixelColor(i, c);
 	}
+
 	strip.show();  //turn pixels on
 
 	delay(wait);
@@ -63,6 +65,7 @@ void flickYellow(uint32_t c, uint8_t wait, uint8_t p)
 	{
 		strip.setPixelColor(i, c);
 	}
+	
 	strip.show();  //turn pixels on
 
 	delay(wait);
